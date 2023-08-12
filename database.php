6378e4b52933cce00332922acff3aad703b6d313
Lurian Vargas <120066573+LURIANMOSCATO@users.noreply.github.com>
2023-08-12 10:24:30 -0300

    att

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
-define("SERVER","localhost:3307");
-define("USER","admin");
-define("PASSWORD","admin");
+define("SERVER","node143512-barber97.jelastic.saveincloud.net");
+define("USER","root");
+define("PASSWORD","TOFdrg19957");
 define("DATABASE","barber");
 ?>
